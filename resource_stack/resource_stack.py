ab67768228f2f030cf8d507c8ac7cf039aca13c1
SRKKakarla <sivakakarla777@gmail.com>
2025-02-13 11:25:41 -0500

    second commit

--- resource_stack/resource_stack.py
+++ resource_stack/resource_stack.py
@@ -24,5 +24,5 @@ class Resource_Stack(Stack):
                                       handler="demo_lambda.lambda_handler")
 
         bucket=s3.Bucket(self, "MyFirstBucket", versioned=True,
-                         bucket_name="demo-bucket",
+                         bucket_name="new-bucket",
                          block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
